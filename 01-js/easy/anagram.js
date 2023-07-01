ceb19979c9018e01ea58bf2c554649ba8f195545
nabeelhashim <nabeelhashim.dev@gmail.com>
2023-07-02 00:16:43 +0530

    isAnagram implementation

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,32 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
 
+    return(false);
+    
+  } else {
+    let matchIndex = 0;
+    for(let i = 0; i < str1.length; i++) {
+      var matchArray = [];
+      for (let j = 0; j < str1.length; j++) {
+        if (str1.charAt(i) == str2.charAt(j)) {
+          matchArray.push(str2.charAt(j));
+        }
+      }
+      if(matchArray.length > 0) {
+        matchIndex++;
+      }
+    }
+    if (matchIndex == str1.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+
+let result = isAnagram("dfffg", "ffdgf");
+console.log(result);
 
 module.exports = isAnagram;
