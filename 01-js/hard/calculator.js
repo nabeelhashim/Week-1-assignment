bfe7d82b75d9d8a34ce22d25df79764edebea137
nabeelhashim <nabeelhashim.dev@gmail.com>
2023-07-02 11:06:14 +0530

    Initial commit

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,163 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  result = 0;
+
+  add(number) {
+    this.result = this.result + number;
+  }
+  subtract(number) {
+    this.result = this.result - number;
+  }
+  multiply(number) {
+    this.result = this.result * number;
+  }
+  divide(number) {
+    this.result = this.result / number;
+  }
+  clear(number) {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(expression) {
+    // Remove any spaces from the expression
+    expression = expression.replace(/\s+/g, "");
+
+    if (!checkIfValidExpression(expression)) {
+      throw new Error("Unbalanced parentheses in the expression");
+    }
+
+    this.result = evaluateExpression(expression);
+    console.log("final result : " + this.result);
+  }
+}
+
+function evaluateExpression(expression) {
+  let values = [];
+  let operators = [];
+  let i = 0;
+
+  while (i < expression.length) {
+    console.log("value : " + values )
+    let char = expression[i];
+
+    // console.log(expression[i]);
+    if (char === "(") {
+      operators.push(char);
+    } else if (!isNaN(char)) {
+      let num = "";
+      while (!isNaN(expression[i]) || expression[i] === ".") {
+        num += expression[i];
+        i++;
+      }
+      values.push(parseFloat(num));
+      continue;
+    } else if (char === ")") {
+      while (operators.length > 0 && expression[operators.length - 1] != "(") {
+        let secondElement = values.pop();
+        let fistElement = values.pop();
+        let operator = operators.pop();
+        values.push(execupeOperation(fistElement, secondElement, operator));
+      }
+      operators.pop();
+    } else {
+      while (
+        operators.length !== 0 &&
+        getPriority(char) <= getPriority(expression[expression.length - 1])
+      ) {
+        let secondElement = values.pop();
+        let firstElement = values.pop();
+        let operator = operators.pop();
+        values.push(execupeOperation(firstElement, secondElement, operator));
+      }
+      operators.push(char);
+    }
+    i++;
+  }
+
+  while (operators.length !== 0) {
+    let secondElement = values.pop();
+    let firstElement = values.pop();
+    let operator = operators.pop();
+    values.push(execupeOperation(firstElement, secondElement, operator));
+  }
+  return values[0];
+}
+
+function getPriority(operator) {
+  switch (operator) {
+    case "+":
+    case "-":
+      return 1;
+    case "*":
+    case "/":
+      return 2;
+    default:
+      return 0;
+  }
+}
+
+function execupeOperation(fistElement, secondElement, operator) {
+  console.log("fistElement : " + fistElement );
+  console.log("secondElement : " +  secondElement);
+  console.log("operator : " + operator);
+  switch (operator) {
+    case "+":
+      return fistElement + secondElement;
+      break;
+    case "-":
+      return fistElement - secondElement;
+      break;
+    case "*":
+      return fistElement * secondElement;
+      break;
+    case "/":
+      if(fistElement !== 0 && secondElement !== 0) {
+        console.log("result : " + fistElement / secondElement);
+        return fistElement / secondElement;
+      } else {
+        console.log("error ");
+        throw new Error("not able to divide");
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+function checkIfValidExpression(expression) {
+  let matchStack = 0;
+  let open = "(";
+  let close = ")";
+
+  for (let element of expression) {
+    console.log(element);
+    if (element == open) {
+      matchStack++;
+    }
+    if (element == close) {
+      matchStack--;
+    }
+  }
+  if (matchStack == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let calculate = new Calculator();
+
+calculate.calculate("(10 + 0)");
+// let sum = calculate.add(5);
+// calculate.add(5);
+// calculate.multiply(10);
+// console.log(calculate.getResult());
+// calculate.clear();
+// console.log(calculate.getResult());
 
 module.exports = Calculator;
